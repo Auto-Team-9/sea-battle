5a41eac9685f65fab9bb7177e43495454a009398
heyArtik-dev <heyArtik@rambler.ru>
2026-02-26 22:58:29 +0300

    fix: move workflow to correct location

--- sea-of-code/src/pages/auth/register.tsx
+++
@@ -1,5 +0,0 @@
-const Register = () => {
-  return <h1>Hello Register</h1>;
-};
-
-export default Register;
--- sea-of-code/src/pages/home-page/homePage.tsx
+++
@@ -1,5 +0,0 @@
-const HomePage = () => {
-  return <section className='flex-1'>Home Page</section>;
-};
-
-export default HomePage;
