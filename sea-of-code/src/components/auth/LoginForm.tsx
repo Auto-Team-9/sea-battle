f4c9b35d7928eb4ab59171698850941d57ff1416
maksim.zhidkov <vjmax793@gmail.com>
2026-03-10 00:00:35 +0300

    feat: add login validation

--- sea-of-code/src/components/auth/LoginForm.tsx
+++ sea-of-code/src/components/auth/LoginForm.tsx
@@ -1,39 +1,114 @@
+import { useState } from 'react';
 import Button from '../ui/Button';
 import Input from '../ui/Input';
 
-const LoginForm = () => (
-  <form className='space-y-6'>
-    <Input label='Email address' name='email' type='email' autoComplete='email' required />
+type FormData = {
+  email: string;
+  password: string;
+};
 
-    <Input
-      label='Password'
-      name='password'
-      type='password'
-      autoComplete='current-password'
-      required
-    />
+type Errors = {
+  email?: string;
+  password?: string;
+};
 
-    <div className='flex items-center justify-between gap-5'>
-      <div className='flex items-center'>
-        <input
-          id='remember-me'
-          name='remember-me'
-          type='checkbox'
-          className='h-4 w-4 cursor-pointer'
-        />
+const LoginForm = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: '',
+    password: '',
+  });
 
-        <label htmlFor='remember-me' className='ml-2 block cursor-pointer text-start text-sm'>
-          Remember me
-        </label>
+  const [errors, setErrors] = useState<Errors>({});
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    setErrors({
+      ...errors,
+      [name]: undefined,
+    });
+  };
+
+  const handleSubmit = (event: React.SubmitEvent) => {
+    event.preventDefault();
+
+    const validationErrors = validate(formData);
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    console.log('Форма:', formData);
+  };
+
+  const validate = (formData: FormData): Errors => {
+    const newErrors: Errors = {};
+
+    if (!formData.email.trim()) {
+      newErrors.email = 'Enter email';
+    } else if (!formData.email.includes('@')) {
+      newErrors.email = 'Incorrect email';
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = 'Enter password';
+    } else if (formData.password.length < 6) {
+      newErrors.password = 'Minimum 6 characters';
+    }
+
+    return newErrors;
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='space-y-4' noValidate>
+      <Input
+        label='Email address'
+        name='email'
+        type='email'
+        onChange={handleChange}
+        error={errors.email}
+        autoComplete='email'
+        required
+      />
+
+      <Input
+        label='Password'
+        name='password'
+        type='password'
+        onChange={handleChange}
+        error={errors.password}
+        autoComplete='current-password'
+        required
+      />
+
+      <div className='flex items-center justify-between gap-5'>
+        <div className='flex items-center'>
+          <input
+            id='remember-me'
+            name='remember-me'
+            type='checkbox'
+            className='h-4 w-4 cursor-pointer'
+          />
+
+          <label htmlFor='remember-me' className='ml-2 block cursor-pointer text-start text-sm'>
+            Remember me
+          </label>
+        </div>
+
+        <a className='text-end text-sm font-medium text-indigo-400 hover:text-indigo-500' href='#'>
+          Forgot password?
+        </a>
       </div>
 
-      <a className='text-end text-sm font-medium text-indigo-400 hover:text-indigo-500' href='#'>
-        Forgot password?
-      </a>
-    </div>
-
-    <Button type='submit'>Sign in</Button>
-  </form>
-);
+      <Button type='submit'>Sign in</Button>
+    </form>
+  );
+};
 
 export default LoginForm;
