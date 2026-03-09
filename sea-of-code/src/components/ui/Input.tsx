b3829686760991883599935b249342997f7d22b1
maksim.zhidkov <vjmax793@gmail.com>
2026-03-10 00:00:15 +0300

    feat: add input error

--- sea-of-code/src/components/ui/Input.tsx
+++ sea-of-code/src/components/ui/Input.tsx
@@ -3,23 +3,26 @@ import type { InputHTMLAttributes, FC } from 'react';
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   name: string;
+  error?: string;
 }
 
-const Input: FC<InputProps> = ({ label, name, className = '', ...props }) => {
+const Input: FC<InputProps> = ({ label, name, error, ...props }) => {
   return (
     <div>
       <label htmlFor={name} className='block text-sm font-medium'>
         {label}
       </label>
 
-      <div className='mt-2'>
+      <div className='my-2'>
         <input
           id={name}
           name={name}
-          className={`block w-full rounded-md bg-white/50 px-3 py-1.5 text-base outline-0 sm:text-sm dark:bg-black/10 ${className}`}
+          className={`block w-full rounded-md bg-white/50 px-3 py-1.5 text-base outline-0 sm:text-sm dark:bg-black/10 ${error ? 'outline-2 -outline-offset-[8px] outline-rose-500 outline-dashed' : ''}`}
           {...props}
         />
       </div>
+
+      {error && <p className='block text-sm font-medium text-rose-500'>{error}</p>}
     </div>
   );
 };
