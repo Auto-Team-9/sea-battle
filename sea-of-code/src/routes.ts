328443fac6a6c7bfcb161f991a308ba2b01d7fad
Andrey <heyartik@rambler.ru>
2026-03-22 17:33:57 +0300

    fix: update routes

--- sea-of-code/src/routes.ts
+++ sea-of-code/src/routes.ts
@@ -6,40 +6,7 @@ import HomePage from './pages/home-page/homePage';
 import Profile from './pages/profile/profile';
 import Game from './pages/game/game';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      Component: RootLayout,
-      children: [
-        {
-          index: true,
-          Component: HomePage,
-        },
-        {
-          path: 'profile',
-          Component: Profile,
-        },
-        {
-          path: 'game',
-          Component: Game,
-        },
-      ],
-    },
-    {
-      path: '/auth',
-      children: [
-        {
-          path: 'login',
-          Component: Login,
-        },
-        {
-          path: 'register',
-          Component: Register,
-        },
-      ],
-    },
-  ],
+const router = createBrowserRouter([
   {
     path: '/Auto-Team-9-Widget-Trainer',
     Component: RootLayout,
