1b1caf871f34667e3b035a429310fe64b62bb42d
maksim.zhidkov <vjmax793@gmail.com>
2026-03-22 21:26:38 +0300

    feat: add login function

--- sea-of-code/src/pages/auth/components/LoginForm.tsx
+++ sea-of-code/src/pages/auth/components/LoginForm.tsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 import Input from '../../../components/ui/Input';
 import Button from '../../../components/ui/Button';
+import { loginUser } from '../../../api/auth';
+import Loading from '../../../components/loading/loading';
+import { auth } from '../../../firebase/config';
+import {
+  browserLocalPersistence,
+  browserSessionPersistence,
+  sendPasswordResetEmail,
+  setPersistence,
+} from 'firebase/auth';
+import Message from '../../../components/ui/Message';
+import { FirebaseError } from 'firebase/app';
+
+type MessageType = 'error' | 'success' | null;
 
 type FormData = {
   email: string;
   password: string;
+  rememberMe: boolean;
 };
 
 type Errors = {
@@ -16,32 +30,109 @@ const LoginForm = () => {
   const [formData, setFormData] = useState<FormData>({
     email: '',
     password: '',
+    rememberMe: false,
   });
 
   const [errors, setErrors] = useState<Errors>({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState<{
+    type: MessageType;
+    text: string;
+  }>({
+    type: null,
+    text: '',
+  });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
+    const { name, value, type, checked } = event.target;
 
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: type === 'checkbox' ? checked : value,
     });
 
     setErrors({
       ...errors,
       [name]: undefined,
     });
+
+    if (message.type) {
+      setMessage({ type: null, text: '' });
+    }
   };
 
-  const handleSubmit = (event: React.SubmitEvent) => {
+  const handleSubmit = async (event: React.SubmitEvent) => {
     event.preventDefault();
 
-    const validationErrors = validate(formData);
+    if (isLoading) return;
 
+    if (message.type) {
+      setMessage({ type: null, text: '' });
+    }
+
+    const validationErrors = validate(formData);
     if (Object.keys(validationErrors).length > 0) {
       setErrors(validationErrors);
       return;
+    }
+
+    setIsLoading(true);
+
+    try {
+      await setPersistence(
+        auth,
+        formData.rememberMe ? browserLocalPersistence : browserSessionPersistence
+      );
+
+      const result = await loginUser({
+        email: formData.email,
+        password: formData.password,
+      });
+
+      if (result.success) {
+        setFormData({
+          email: '',
+          password: '',
+          rememberMe: false,
+        });
+      } else {
+        setMessage({
+          type: 'error',
+          text: result.error.message,
+        });
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleResetPassword = async () => {
+    if (!formData.email.trim()) {
+      setErrors(prev => ({
+        ...prev,
+        email: 'Enter email to reset password',
+      }));
+      return;
+    }
+
+    try {
+      await sendPasswordResetEmail(auth, formData.email);
+
+      setMessage({
+        type: 'success',
+        text: 'Check your email for reset link',
+      });
+    } catch (error: unknown) {
+      let errorMessage = 'Something went wrong';
+
+      if (error instanceof FirebaseError) {
+        errorMessage = error.message;
+      }
+
+      setMessage({
+        type: 'error',
+        text: errorMessage,
+      });
     }
   };
 
@@ -65,32 +156,38 @@ const LoginForm = () => {
 
   return (
     <form onSubmit={handleSubmit} className='space-y-4' noValidate>
+      {isLoading && <Loading className='fixed' />}
+
+      {message.type && <Message variant={message.type} size='small' message={message.text} />}
+
       <Input
         label='Email address'
         name='email'
         type='email'
         onChange={handleChange}
+        value={formData.email}
         error={errors.email}
         autoComplete='email'
         required
       />
-
       <Input
         label='Password'
         name='password'
         type='password'
         onChange={handleChange}
+        value={formData.password}
         error={errors.password}
         autoComplete='current-password'
         required
       />
-
       <div className='flex items-center justify-between gap-5'>
         <div className='flex items-center'>
           <input
             id='remember-me'
-            name='remember-me'
+            name='rememberMe'
             type='checkbox'
+            checked={formData.rememberMe}
+            onChange={handleChange}
             className='h-4 w-4 cursor-pointer'
           />
 
@@ -99,11 +196,10 @@ const LoginForm = () => {
           </label>
         </div>
 
-        <a className='text-end text-sm font-medium text-indigo-400 hover:text-indigo-500' href='#'>
+        <Button type='button' variant='link' onClick={handleResetPassword}>
           Forgot password?
-        </a>
+        </Button>
       </div>
-
       <Button type='submit'>Sign in</Button>
     </form>
   );
