fd140870c3e744054b9c194101b7421aec250757
maksim.zhidkov <vjmax793@gmail.com>
2026-03-22 21:23:41 +0300

    refactor: use authPage component

--- sea-of-code/src/pages/auth/login.tsx
+++ sea-of-code/src/pages/auth/login.tsx
@@ -1,25 +1,15 @@
 import LoginForm from './components/LoginForm';
-import AuthHeader from './components/AuthHeader';
-import OAuthButtons from './components/OAuthButtons';
-import AuthLink from './components/AuthLink';
+import AuthPage from './components/AuthPage';
 
 const Login = () => {
   return (
-    <div className='flex min-h-full flex-col justify-center self-center'>
-      <AuthHeader title={'Sign in, commander'} />
-
-      <div className='doodle doodle-border mt-5 rounded-xl bg-white/50 p-3 sm:mx-auto sm:mt-7 sm:w-full sm:max-w-sm sm:p-6 dark:bg-black/10'>
-        <LoginForm />
-
-        <OAuthButtons />
-
-        <AuthLink
-          text={"Don't have an account?"}
-          linkText={'Create Account'}
-          linkTo={'/Auto-Team-9-Widget-Trainer/auth/register'}
-        />
-      </div>
-    </div>
+    <AuthPage
+      title='Sign in, commander'
+      form={<LoginForm />}
+      linkText="Don't have an account?"
+      linkLabel='Create Account'
+      linkTo='/auth/register'
+    />
   );
 };
 
--- sea-of-code/src/pages/auth/register.tsx
+++ sea-of-code/src/pages/auth/register.tsx
@@ -1,25 +1,15 @@
-import AuthHeader from './components/AuthHeader';
-import AuthLink from './components/AuthLink';
-import OAuthButtons from './components/OAuthButtons';
+import AuthPage from './components/AuthPage';
 import RegisterForm from './components/RegisterForm';
 
 const Register = () => {
   return (
-    <div className='flex min-h-full flex-col justify-center self-center'>
-      <AuthHeader title={'New commander'} />
-
-      <div className='doodle doodle-border mt-5 rounded-xl bg-white/50 p-3 sm:mx-auto sm:mt-7 sm:w-full sm:max-w-sm sm:p-6 dark:bg-black/10'>
-        <RegisterForm />
-
-        <OAuthButtons />
-
-        <AuthLink
-          text={'Already have an account?'}
-          linkText={'Login'}
-          linkTo={'/Auto-Team-9-Widget-Trainer/auth/login'}
-        />
-      </div>
-    </div>
+    <AuthPage
+      title='New commander'
+      form={<RegisterForm />}
+      linkText='Already have an account?'
+      linkLabel='Login'
+      linkTo='/auth/login'
+    />
   );
 };
 
