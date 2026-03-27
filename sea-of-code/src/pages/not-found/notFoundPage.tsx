68db17f362093a647347c0111b16071b6ae8f050
maksim.zhidkov <vjmax793@gmail.com>
2026-03-27 17:35:37 +0300

    feat: 404 page adjust

--- sea-of-code/src/pages/not-found/notFoundPage.tsx
+++ sea-of-code/src/pages/not-found/notFoundPage.tsx
@@ -8,11 +8,17 @@ const NotFoundPage = () => {
     navigate(-1);
   };
   return (
-    <div className='flex w-full flex-col items-center justify-center p-4'>
-      <div className='doodle-border flex w-1/2 flex-col items-center justify-center gap-5 p-4 text-center text-5xl'>
-        <h2 className='text-7xl font-bold'>404</h2>
-        <p className='mt-4 text-2xl'>We couldn't find the page you were looking for.</p>
-        <Button onClick={handleGoBack} className={'!w-3xs'}>
+    <div className='doodle flex w-full flex-col items-center justify-center !bg-transparent p-4'>
+      <div className='doodle-border flex w-1/2 flex-col items-center justify-center gap-6 p-4 text-center'>
+        <div>
+          <h2 className='text-7xl font-bold'>404</h2>
+          <p className='text-2xl'>Sorry :(</p>
+        </div>
+
+        <p className='text-2xl'>
+          We couldn't find the page you were looking for. Visit our Home Page for a fresh start.
+        </p>
+        <Button variant='primary' onClick={handleGoBack} className={'!w-3xs'}>
           Go back
         </Button>
       </div>
