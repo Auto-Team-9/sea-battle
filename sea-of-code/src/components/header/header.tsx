96ab74b1817e9586e6e76311e29383a7f38b3f35
maksim.zhidkov <vjmax793@gmail.com>
2026-03-27 22:28:45 +0300

    feat: add header layout

--- sea-of-code/src/components/header/header.tsx
+++ sea-of-code/src/components/header/header.tsx
@@ -1,28 +1,79 @@
 import { NavLink } from 'react-router';
+import logo from '../../assets/battleship-alt.svg';
+import { useAuth } from '../../firebase/useAuth';
+import { logoutUser } from '../../api/auth';
+import { defaultAvatar, profileImage } from '../../constants/images';
+import { useRef } from 'react';
+import Button from '../ui/Button';
+import logoutIcon from '../../assets/logout-icon.svg';
+import ThemeButton from '../ui/ThemeButton';
 
 const Header = () => {
+  const { loading, userData } = useAuth();
+  const imgRef = useRef<HTMLImageElement>(null);
+
+  if (loading || !userData) return <></>;
+
+  const avatar = localStorage.getItem(profileImage) || defaultAvatar;
+  const displayName = userData.displayName || 'player';
+
+  const linkClass = ({ isActive }: { isActive: boolean }) =>
+    `ease-in-out hover:text-indigo-500 transition duration-300 ${isActive ? 'text-indigo-500 cursor-default' : ''}`;
+
   return (
-    <header className='p-4'>
-      <nav className='flex items-center justify-between'>
-        <div className='h-16 w-16 cursor-pointer transition-transform hover:scale-120'>
-          <img src='./battleship.png' alt='logo' />
+    <header className='doodle-border m-4 flex items-center justify-between p-2 px-5'>
+      <NavLink
+        to='/'
+        className='flex w-fit cursor-pointer items-center gap-3 transition-transform hover:text-indigo-500'
+      >
+        <img src={logo} alt='Logo' className='h-14' />
+        <span className='text-3xl font-bold transition duration-300'>Sea of Code</span>
+      </NavLink>
+
+      <div className='flex items-center justify-between gap-10'>
+        <nav className='flex items-center justify-between'>
+          <ul className='flex gap-5 text-xl'>
+            <NavLink to={'/about'} className={linkClass}>
+              About
+            </NavLink>
+            <NavLink to={'/clans'} className={linkClass}>
+              Clans
+            </NavLink>
+            <NavLink to={'/Profile'} className={linkClass}>
+              Profile
+            </NavLink>
+          </ul>
+        </nav>
+
+        <div className='doodle-vr h-15' />
+
+        <div className='flex gap-5'>
+          <ThemeButton />
+
+          <Button
+            variant='round'
+            onClick={logoutUser}
+            icon={<img src={logoutIcon} alt='logout icon' className='h-9 w-9' />}
+            className='bg-red-400 hover:bg-red-500'
+          ></Button>
         </div>
-        <h1 className='text-4xl'>Sea of Code</h1>
-        <ul className='flex gap-4 text-xl'>
-          <NavLink
-            to={'/auth/register'}
-            className='rounded-xl p-2 transition-colors hover:bg-blue-600'
-          >
-            Register
-          </NavLink>
-          <NavLink
-            to={'/auth/login'}
-            className='rounded-xl p-2 transition-colors hover:bg-blue-600'
-          >
-            Login
-          </NavLink>
-        </ul>
-      </nav>
+
+        <div className='doodle-vr h-15' />
+
+        <div className='flex items-center gap-5'>
+          <div className='relative h-15 w-15'>
+            <img
+              ref={imgRef}
+              src={avatar}
+              alt='avatar'
+              className='h-full w-full scale-90 object-cover'
+            />
+            <div className='doodle-border pointer-events-none absolute inset-0 !bg-transparent'></div>
+          </div>
+
+          <span className='text-center text-3xl sm:text-3xl'>{displayName}</span>
+        </div>
+      </div>
     </header>
   );
 };
