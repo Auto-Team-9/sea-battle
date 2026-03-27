9e9e272737c0498ff01e1d77d5ce8c7458ed1a61
maksim.zhidkov <vjmax793@gmail.com>
2026-03-27 22:29:04 +0300

    feat: add main page loading state

--- sea-of-code/src/pages/home-page/homePage.tsx
+++ sea-of-code/src/pages/home-page/homePage.tsx
@@ -1,4 +1,11 @@
+import Loading from '../../components/ui/loading';
+import { useAuth } from '../../firebase/useAuth';
+
 const HomePage = () => {
+  const { loading } = useAuth();
+
+  if (loading) return <Loading />;
+
   return <section className='flex-1'>Home Page</section>;
 };
 
