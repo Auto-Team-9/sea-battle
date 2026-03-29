e49e4b4d6fa28332605cb62493aae57e29e7a8f1
maksim.zhidkov <vjmax793@gmail.com>
2026-03-30 00:04:56 +0300

    feat: add animated assistant picture

---
+++ sea-of-code/src/pages/home-page/components/RandomSailor.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { DizzySailor } from './DizzySailor';
+import { SickSailor } from './SickSailor';
+import { SmokingSailor } from './Smoker';
+import { ThinkingSailor } from './Thinkier';
+
+const RandomSailor = () => {
+  const sailors = [SmokingSailor, ThinkingSailor, DizzySailor, SickSailor];
+
+  const [RandomSailor] = useState(() => sailors[Math.floor(Math.random() * sailors.length)]);
+
+  return (
+    <div className='relative h-25 w-25'>
+      <div className='relative scale-94 !bg-white'>
+        <RandomSailor />
+      </div>
+      <div className='doodle-border absolute inset-0 !bg-transparent'></div>
+    </div>
+  );
+};
+
+export default RandomSailor;
---
+++ sea-of-code/src/pages/home-page/components/Smoker.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import smokeIcon from '../../../assets/smoke.svg';
+import smokeAltIcon from '../../../assets/smoke-alt.svg';
+import smokingIcon from '../../../assets/smoking.svg';
+
+export function SmokingSailor() {
+  const [frame, setFrame] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setFrame(frame => (frame + 1) % 2);
+    }, 1500);
+    return () => clearInterval(timer);
+  }, []);
+
+  return (
+    <>
+      <img className='h-fit w-fit !bg-transparent' src={smokingIcon} alt='Smoking sailor icon' />
+
+      <div className='absolute top-0 right-0 h-25 w-25 animate-[pulse_3s_ease-in-out_infinite]'>
+        <img
+          className={`absolute top-0 right-0 h-fit w-fit !bg-transparent transition-opacity`}
+          src={smokeIcon}
+          alt='Smoke frame'
+          aria-hidden='true'
+          style={{ opacity: frame === 0 ? 1 : 0 }}
+        />
+        <img
+          className={`absolute -top-1 right-0 h-fit w-fit !bg-transparent transition-opacity`}
+          src={smokeAltIcon}
+          alt='Smoke frame'
+          aria-hidden='true'
+          style={{ opacity: frame === 1 ? 1 : 0 }}
+        />
+      </div>
+    </>
+  );
+}
