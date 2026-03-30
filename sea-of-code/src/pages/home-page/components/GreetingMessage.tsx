dcb0f348d55d754c503f8cbaf15e7965eb33840f
maksim.zhidkov <vjmax793@gmail.com>
2026-03-30 14:06:27 +0300

    feat: greeting style adjustments

--- sea-of-code/src/pages/home-page/components/GreetingMessage.tsx
+++ sea-of-code/src/pages/home-page/components/GreetingMessage.tsx
@@ -28,21 +28,20 @@ const GreetingMessage = ({
   return (
     <div className='relative flex h-full w-full'>
       <div
-        className='absolute flex h-full w-full items-center justify-center transition-opacity duration-700'
+        className='absolute flex h-full w-full flex-col items-center justify-center transition-opacity duration-700'
         style={{ opacity: showMain ? 0 : 1 }}
       >
-        <p className='text-2xl'>
-          {getGreeting}, {displayName}
-        </p>
+        <p className='text-2xl'>{getGreeting},</p>
+        <p className='text-2xl'>{displayName}</p>
       </div>
 
       <div
-        className='flex h-full w-full flex-col justify-center gap-1 transition-opacity duration-700'
+        className='gap- flex h-full w-full flex-col justify-center transition-opacity duration-700'
         style={{ opacity: showMain ? 1 : 0 }}
       >
-        <p className='text-sm'>{intro}</p>
+        <p>{intro}</p>
 
-        <p className='text-sm'>{main}</p>
+        <p>{main}</p>
 
         <p className='text-sm text-[--color-text] opacity-70'>
           {to_rank > 0 ? `${to_rank} XP to the next rank` : 'Maximum rank reached'}
