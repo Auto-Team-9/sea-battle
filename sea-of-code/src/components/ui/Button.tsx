d2c95389519f77231bab2cfc856520ef78f4b22a
maksim.zhidkov <vjmax793@gmail.com>
2026-04-04 16:42:07 +0300

    feat: add round button varian

--- sea-of-code/src/components/ui/Button.tsx
+++ sea-of-code/src/components/ui/Button.tsx
@@ -1,5 +1,6 @@
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
 import ArrowIcon from '../../assets/arrow.svg?react';
+import RoundIcon from '../../styles/vendor/doodleCss/round.svg?react';
 
 type ButtonVariant = 'primary' | 'secondary' | 'link' | 'round' | 'arrow';
 
@@ -15,18 +16,21 @@ const variantStyles: Record<ButtonVariant, string> = {
   secondary:
     'bg-[var(--color-btn-secondary)] hover:bg-[var(--color-btn-secondary-hover)] focus-visible:outline-offset-[-8px]',
   link: 'bg-none border-none text-end text-sm font-medium text-[var(--color-accent)] hover:opacity-80 !border-0 inline !p-0 focus-visible:outline-offset-0 rounded-none',
-  round: `hover:bg-[var(--color-btn-primary-hover)] !p-0 doodle-round-button w-fit h-fit !rounded-2xl`,
+  round: `relative hover:bg-[var(--color-btn-primary-hover)] !p-0 doodle-round-button w-fit h-fit !rounded-2xl`,
   arrow: `!p-0 !h-fit !w-fit border-none text-[var(--color-text)] hover:text-[var(--color-btn-primary-hover)] hover:scale-110 `,
 };
 
 const Button = ({ variant = 'primary', icon, className = '', children, ...props }: ButtonProps) => {
   return (
     <button
-      className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border px-3 py-1.5 text-sm font-semibold transition duration-300 ease-in-out focus-visible:outline-2 active:scale-95 ${variantStyles[variant]} ${className}`}
+      className={`flex w-full cursor-pointer items-center justify-center gap-2 rounded-md border px-3 py-1.5 text-sm transition duration-300 ease-in-out focus-visible:outline-2 active:scale-95 ${variantStyles[variant]} ${className}`}
       {...props}
     >
       {variant === 'arrow' ? (
         <ArrowIcon className='h-20 fill-current stroke-current text-current' />
+      ) : null}
+      {variant === 'round' ? (
+        <RoundIcon className='absolute h-fit fill-current text-current opacity-50' />
       ) : null}
       {icon}
       {children}
