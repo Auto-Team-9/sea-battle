c7f52c9122fc28f4e78a81e49ae0fa7e87ede185
maksim.zhidkov <vjmax793@gmail.com>
2026-04-04 16:46:04 +0300

    feat: add topics background

--- sea-of-code/src/pages/home-page/components/MapComponent.tsx
+++ sea-of-code/src/pages/home-page/components/MapComponent.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { Topics } from '../../../types/quiz';
-import {
-  seaPhrases,
-  topicLocations,
-  topicObjectives,
-  topicOperations,
-} from '../../../constants/constants';
+import { seaPhrases } from '../../../constants/constants';
 import Button from '../../../components/ui/Button';
 import LvlSelector from './LvlSelector';
 import { useNavigate } from 'react-router';
+import {
+  topicLocations,
+  topicObjectives,
+  topicOperations,
+  topicPatterns,
+} from '../../../constants/topics';
+
 interface MapProps {
   currentTopic: Topics;
   onNext: () => void;
@@ -22,6 +24,7 @@ const MapComponent = ({ currentTopic, onNext, onPrev }: MapProps) => {
   const operation = topicOperations[currentTopic];
   const location = topicLocations[currentTopic];
   const objective = topicObjectives[currentTopic];
+  const patterns = topicPatterns[currentTopic];
 
   const [randomPhrase] = useState(() => {
     const index = Math.floor(Math.random() * seaPhrases.length);
@@ -37,7 +40,11 @@ const MapComponent = ({ currentTopic, onNext, onPrev }: MapProps) => {
   };
 
   return (
-    <div className='doodle-border relative z-10 flex h-full flex-col justify-between p-4'>
+    <div
+      className={`doodle-border relative z-10 flex h-full flex-col justify-between overflow-hidden p-4`}
+    >
+      {patterns}
+
       <div className='flex w-fit max-w-136 flex-col gap-1'>
         <div className='flex items-center gap-4'>
           <hr className='doodle-hr w-full flex-grow' />
@@ -52,36 +59,32 @@ const MapComponent = ({ currentTopic, onNext, onPrev }: MapProps) => {
           </p>
 
           <p>
-            Objective: <span className='opacity-70'>{objective}</span>{' '}
+            Objective: <span className='opacity-70'>{objective}</span>
           </p>
         </div>
       </div>
-
-      <div className='flex w-full flex-col items-center gap-5 self-end'>
+      <div className={`doodle flex w-fit flex-col items-end gap-5 self-end !bg-transparent`}>
         <Button
           variant='primary'
-          className='z-20 !w-fit'
+          className={`z-20 !w-fit ${selectedLevel ? 'opacity-100' : 'opacity-0'}`}
           disabled={!selectedLevel}
           onClick={startBattle}
         >
-          Battle!
+          To Battle!
         </Button>
 
         <p className='self-center self-end text-center text-sm italic opacity-70'>{randomPhrase}</p>
       </div>
-
       <Button
         variant='arrow'
         onClick={onPrev}
         className='absolute top-1/2 left-0 z-20 ml-4 -translate-y-1/2 rotate-180'
       ></Button>
-
       <Button
         variant='arrow'
         onClick={onNext}
         className='absolute top-1/2 right-0 z-20 mr-4 -translate-y-1/2'
       ></Button>
-
       <LvlSelector
         currentTopic={currentTopic}
         selectedLevel={selectedLevel}
