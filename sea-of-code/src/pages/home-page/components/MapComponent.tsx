7cc35dd7d1a52f843e7e327ff31cb3d920217618
maksim.zhidkov <vjmax793@gmail.com>
2026-04-13 17:12:32 +0300

    feat: add map animations

--- sea-of-code/src/pages/home-page/components/MapComponent.tsx
+++ sea-of-code/src/pages/home-page/components/MapComponent.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, type ReactElement } from 'react';
 import { type Topic } from '../../../types/topic';
 import { seaPhrases } from '../../../constants/constants';
 import Button from '../../../components/ui/Button';
-import LvlSelector from './LvlSelector';
+import LevelSelector from './LevelSelector';
 import { useNavigate } from 'react-router';
 import type { Level } from '../../../types/level';
 import { getLevelsByTopic } from '../../../api/levels';
 import { useAuth } from '../../../firebase/useAuth';
+import Loading from '../../../components/ui/loading';
+import { AnimatePresence, motion } from 'motion/react';
 
 interface MapProps {
   currentTopic: Topic;
@@ -19,12 +21,19 @@ const MapComponent = ({ currentTopic, onNext, onPrev }: MapProps) => {
   const navigate = useNavigate();
   const [selectedLevelId, setSelectedLevel] = useState<string | null>(null);
   const [levels, setLevels] = useState<Level[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const loadLevels = async () => {
-      const data = await getLevelsByTopic(currentTopic.id);
-      data.sort((a, b) => a.number - b.number);
-      setLevels(data);
+      setIsLoading(true);
+
+      try {
+        const data = await getLevelsByTopic(currentTopic.id);
+        data.sort((a, b) => a.number - b.number);
+        setLevels(data);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     loadLevels();
@@ -48,59 +57,99 @@ const MapComponent = ({ currentTopic, onNext, onPrev }: MapProps) => {
     });
   };
 
-  return (
-    <div
-      className={`doodle-border relative z-10 flex h-full flex-col justify-between overflow-hidden p-4`}
-    >
-      {background}
-
-      <div className='flex w-fit max-w-136 flex-col gap-1'>
-        <div className='flex items-center gap-4'>
-          <hr className='doodle-hr w-full flex-grow' />
-          <p className='tracking-wide whitespace-nowrap uppercase opacity-70'>Selected Operation</p>
-          <hr className='doodle-hr w-full flex-grow rotate-180 transform' />
-        </div>
-        <div>
-          <p className='text-3xl font-semibold'>{operation}</p>
-
-          <p>
-            Location: <span className='opacity-70'>{location}</span>
-          </p>
-
-          <p>
-            Objective: <span className='opacity-70'>{objective}</span>
-          </p>
-        </div>
-      </div>
-      <div className={`doodle flex w-fit flex-col items-end gap-5 self-end !bg-transparent`}>
-        <Button
-          variant='primary'
-          className={`z-20 !w-fit ${selectedLevelId ? 'opacity-100' : 'opacity-0'}`}
-          disabled={!selectedLevelId}
-          onClick={startBattle}
+  const textCustomAnimation = (key: string, component: ReactElement) => {
+    return (
+      <AnimatePresence mode='wait'>
+        <motion.div
+          key={key}
+          initial={{ opacity: 0, filter: 'blur(3px)' }}
+          animate={{ opacity: 1, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, filter: 'blur(3px)' }}
+          transition={{ duration: 0.3, ease: 'easeOut' }}
+          className='inline'
         >
-          To Battle!
-        </Button>
+          {component}
+        </motion.div>
+      </AnimatePresence>
+    );
+  };
 
-        <p className='self-center self-end text-center text-sm italic opacity-70'>{randomPhrase}</p>
+  return (
+    <AnimatePresence mode='wait'>
+      <div
+        className={`doodle-border relative z-10 flex h-full flex-col justify-between overflow-hidden p-4`}
+      >
+        {!isLoading ? (
+          <LevelSelector
+            levels={levels}
+            completedLevels={userData?.stats.completedLevels ?? []}
+            selectedLevel={selectedLevelId}
+            onSelectLevel={setSelectedLevel}
+          />
+        ) : (
+          <Loading className='t-1/2 l-1-2 absolute bg-transparent' />
+        )}
+
+        <motion.div
+          key={operation}
+          initial={{ opacity: 0, filter: 'blur(3px)' }}
+          animate={{ opacity: 1, filter: 'blur(0px)' }}
+          exit={{ opacity: 0, filter: 'blur(3px)' }}
+          transition={{ duration: 0.3, ease: 'easeOut' }}
+          className='absolute inset-0 -z-20 h-full w-full'
+        >
+          {background}
+        </motion.div>
+
+        <div className='flex w-fit max-w-136 flex-col gap-1'>
+          <div className='flex items-center gap-4'>
+            <hr className='doodle-hr w-full flex-grow' />
+            <p className='tracking-wide whitespace-nowrap uppercase opacity-70'>
+              Selected Operation
+            </p>
+            <hr className='doodle-hr w-full flex-grow rotate-180 transform' />
+          </div>
+
+          <div>
+            {textCustomAnimation(operation, <p className='text-3xl font-semibold'>{operation}</p>)}
+
+            <p>
+              Location:{' '}
+              {textCustomAnimation(location, <span className='opacity-70'>{location}</span>)}
+            </p>
+
+            <p>
+              Objective:{' '}
+              {textCustomAnimation(objective, <span className='opacity-70'>{objective}</span>)}
+            </p>
+          </div>
+        </div>
+        <div className={`doodle flex w-fit flex-col items-end gap-5 self-end !bg-transparent`}>
+          <Button
+            variant='primary'
+            className={`z-20 !w-fit ${selectedLevelId ? 'opacity-100' : 'opacity-0'}`}
+            disabled={!selectedLevelId}
+            onClick={startBattle}
+          >
+            To Battle!
+          </Button>
+
+          <p className='self-center self-end text-center text-sm italic opacity-70'>
+            {randomPhrase}
+          </p>
+        </div>
+        <Button
+          variant='arrow'
+          onClick={onPrev}
+          className='absolute top-1/2 left-0 z-20 ml-4 -translate-y-1/2 rotate-180'
+        ></Button>
+        <Button
+          variant='arrow'
+          onClick={onNext}
+          className='absolute top-1/2 right-0 z-20 mr-4 -translate-y-1/2'
+        ></Button>
       </div>
-      <Button
-        variant='arrow'
-        onClick={onPrev}
-        className='absolute top-1/2 left-0 z-20 ml-4 -translate-y-1/2 rotate-180'
-      ></Button>
-      <Button
-        variant='arrow'
-        onClick={onNext}
-        className='absolute top-1/2 right-0 z-20 mr-4 -translate-y-1/2'
-      ></Button>
-      <LvlSelector
-        levels={levels}
-        completedLevels={userData?.stats.completedLevels ?? []}
-        selectedLevel={selectedLevelId}
-        onSelectLevel={setSelectedLevel}
-      />
-    </div>
+    </AnimatePresence>
   );
 };
 
